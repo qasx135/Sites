8454db35b411bad9ada98b3c8ba9096b0be0eda5
qasx135 <qasx135@gmail.com>
2023-11-26 16:08:19 +0300

    Done12

---
+++ 12/js.js
@@ -0,0 +1,80 @@
+let notifications = document.querySelector(".notifications12")
+let mainInterface = document.querySelector(".interUl")
+let delNot = document.querySelector(".deleteNotif")
+let count1 = 0
+let clicked = false
+let option = 'Hello for 1.5 second'
+let timer
+
+
+// let interf = {
+//     'input': []
+// }
+
+// function interface() {
+//     let timer = setInterval(addItem, 1000)
+//     function addItem() {
+//         let item = prompt("XUI")
+//         if (item != '' && item != null && item != undefined){
+//             let div = document.createElement("li")
+//             div.innerHTML = item
+//             div.classList.add("addedInter")
+//             mainInterface.appendChild(div)
+//         } else {
+//             clearInterval(timer)
+//         }
+//     }
+    
+// }
+
+
+// function showNotificztion(option) {
+//     let div = document.createElement("div")
+//     div.innerHTML = option
+//     div.classList.add("notificationDelete")
+//     delNot.appendChild(div)
+//     setTimeout(function() {
+//         div.style.display = 'none'
+//     }, 1500)
+// }
+
+// showNotificztion(option)
+//---------------------------------------------------------------//
+
+
+function addNewNot() {
+    count1++
+    let div = document.createElement("div")
+    div.innerHTML = "MAX PIDORASS"
+    div.classList.add("addedNot")
+    notifications.appendChild(div)
+    console.log(count1)
+}
+
+
+function stopDecorator(func) {
+    return function() {
+        if (clicked){
+            console.log("in decor")
+            clearInterval(timer)
+            timer = setInterval(func, 10000)
+            console.log(clicked)
+        } else {
+            console.log("almost in dec")
+            timer = setInterval(func, 3000)
+        }
+            
+    }
+}
+
+addNewNot = stopDecorator(addNewNot)
+addNewNot()
+
+function clicker() {
+    if (clicked)
+        clicked = false
+    else
+        clicked = true
+    console.log("clicked")
+    addNewNot()
+}
